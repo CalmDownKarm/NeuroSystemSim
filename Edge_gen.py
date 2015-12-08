0d6167c62a8d6189a9a0a18c9c8b9737c6ddae6c
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2015-12-08 08:26:27 +0530

    Almost done

--- Edge_gen.py
+++ Edge_gen.py
@@ -1,5 +1,15 @@
-  { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
 
-var i = 1
+i = 1
+target = 1
 while(i<100):
-  
+  source = i
+  string = " { data: { id: '%d%d',weight:%d,source:'%d',target: '%d'}},"%(source,target,0,source,target)
+  print(string)
+  target2 = (i+target)%100
+  string = " { data: { id: '%d%d',weight:%d,source:'%d',target: '%d'}},"%(source,target2,0,source,target2)
+  print(string)
+  #target3 = (target+ target2)%100
+  #string = " { data: { id: '%d%d',weight:%d,source:'%d',target: '%d'}},"%(source,target3,0,source,target3)
+  print(string)
+  target = (target2 + target) %100
+  i+=1
