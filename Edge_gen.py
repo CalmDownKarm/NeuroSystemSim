a653efc8f1ddd2980f1d94a075ed6cd3826e34c6
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2015-12-08 10:42:55 +0530

    Enough for now

--- Edge_gen.py
+++ Edge_gen.py
@@ -3,10 +3,10 @@ i = 1
 target = 1
 while(i<100):
   source = i
-  string = " { data: { id: '%d%d',weight:%d,source:'%d',target: '%d'}},"%(source,target,0,source,target)
+  string = " { data: { id: '%d%d',weight:%d,source:'%d',target: '%d'}},"%(source,target,1,source,target)
   print(string)
   target2 = (i+target)%100
-  string = " { data: { id: '%d%d',weight:%d,source:'%d',target: '%d'}},"%(source,target2,0,source,target2)
+  string = " { data: { id: '%d%d',weight:%d,source:'%d',target: '%d'}},"%(source,target2,1,source,target2)
   print(string)
   #target3 = (target+ target2)%100
   #string = " { data: { id: '%d%d',weight:%d,source:'%d',target: '%d'}},"%(source,target3,0,source,target3)
